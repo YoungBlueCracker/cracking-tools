f530b4f1f4ba1ede82a23df60f00d35ca02b0aaf
YoungBlueCracker <YoungBlueCracker@gmail.com>
2016-06-12 17:23:52 +0100

    Update bruteForcer.py

--- bruteForcer.py
+++ bruteForcer.py
@@ -7,8 +7,8 @@
 #   NOOFCHARACTERS: The number of characters in the password. If unknown, enter -1; this will test every combination of one   #
 #  character, then every combination of two characters, then three, etc. Be careful, this can easily start an infinite loop   #
 #                                               with an incorrect character set.                                              #
-# CHARACTERSET: The character set to use. Use A to test upper case letters, a to test lower case letters, n to test numbers,  #
-#       and s to test special characters. You can use any combination of A, a, n and s, e.g. A, s, sa, Aans, snaA, etc.       #
+#    CHARACTERSET: The character set(s) to use. Use A to test upper case letters, a to test lower case letters, n to test     # 
+#   numbers and s to test special characters. You can use any combination of A, a, n and s, e.g. A, s, sa, Aans, snaA, etc.   #
 #                                       -v: verbose mode (outputs word being attempted)                                       #
 #                                          RUN WITH -h TO VIEW HELP MESSAGE AND EXIT                                          #
 #                                                                                                                             #
@@ -29,7 +29,7 @@ TEXT_COLOUR_RESET = "\033[0m"
 def parse():
 	parser = argparse.ArgumentParser(epilog = "Example: bruteForcer.py 3 Aans passwords.zip -v")
 	parser.add_argument("noOfCharacters", metavar = "<NOOFCHARACTERS>", type = int, help = "How many characters does the password contain? Enter \"-1\" if you don't know (just be careful, this can start an infinite loop very easily and you might need to mash Ctrl+C a few times to exit the program if you use the wrong character set).")
-	parser.add_argument("characterSet", metavar = "<CHARACTERSET>", help = "The character set to use. Type A for uppercase letters, a for lowercase letters, n for numbers, and s for special characters. You can use any combination of A, a, n or s.")
+	parser.add_argument("characterSet", metavar = "<CHARACTERSET>", help = "The character set(s) to use. Type A for uppercase letters, a for lowercase letters, n for numbers, and s for special characters. You can use any combination of A, a, n or s.")
 	parser.add_argument("zipFile", metavar = "<ZIPFILE>", help = "The zip file that we are attempting to crack.")
 	parser.add_argument("-v", "--verbosity", help = "Run program in verbose mode", action = "store_true", default = False)
 	args = parser.parse_args()
